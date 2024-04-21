75194a8d3b2b1d4a55c068117a5d04dc058e8093
Anna H00k <annahook0@gmail.com>
2024-04-22 00:46:04 +0200

    basepath for different circumstances

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+// next.config.js
+
+const nextConfig = {
+    basePath: process.env.NODE_ENV === 'production' ? '/co2-zone' : '',
+};
 
 export default nextConfig;

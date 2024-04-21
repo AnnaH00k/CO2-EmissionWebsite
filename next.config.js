74eafac9884ebd55ae9ce3e98b23a5a956bebc3d
Anna H00k <annahook0@gmail.com>
2024-04-21 02:48:48 +0200

    freecodecamp deploy to github pages changes

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    basePath: "/co2-emissionwebsite",
+    output: "export",
+    reactStrictMode: true,
+  };
+  
+  module.exports = nextConfig;

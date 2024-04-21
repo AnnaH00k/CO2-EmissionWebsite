9412d523c8a674094b4d98fe1ba51f430e7b961b
Anna H00k <annahook0@gmail.com>
2024-04-21 16:24:43 +0200

    try fixing navigation for github pages

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -5,9 +5,9 @@ export default function Footer() {
     return (
     <footer className="flex my-[10vh] mx-2 justify-center">
         <ul className="">
-            <a className="m-2 text-blueExtraLight" href="/datasecurity"> Datasecurity</a>
-            <a className="m-2 text-blueExtraLight"  href="/impressum"> Imprint</a>
-            <a className="m-2 text-blueExtraLight"  href="/contact">Contact</a>
+            <a className="m-2 text-blueExtraLight" href="../datasecurity"> Datasecurity</a>
+            <a className="m-2 text-blueExtraLight"  href="../impressum"> Imprint</a>
+            <a className="m-2 text-blueExtraLight"  href="../contact">Contact</a>
         </ul>
     </footer>
     );

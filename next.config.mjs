256b961a4dfe4a63b797d658bb5a907e16761948
Anna H00k <annahook0@gmail.com>
2024-04-21 23:54:26 +0200

    more routing trys with next.config

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    basePath: "/CO2-EmissionWebsite",
+   
+    images: {
+        domains: ['localhost', '/co2-emissionwebsite'],
+    },
+
+  
 };
 
 export default nextConfig;

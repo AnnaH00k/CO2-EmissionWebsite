b40dc85ccbaacad9682ddca3e5ee018d71641e64
Anna H00k <annahook0@gmail.com>
2024-04-21 23:25:51 +0200

    add base path to next.config

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
       <main>
         <div className=" mt-[5vh] flex flex-col justify-center items-center">
           <div className="w-[40vw] max-w-[500px]">
-            <img src="/CO2-EmissionWebsite/public/CO2zoneLogo.png" alt="logo" width="100%" />
+            <img src="/CO2zoneLogo.png" alt="logo" width="100%" />
           </div>
           <h1 className="flex hyphens-auto text-center text-blueExtraLight text-lg sm:text-4xl m-4 ">
             Advancing Environmental Consciousness, Together!
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    basePath: "/CO2-EmissionWebsite",
+};
 
 export default nextConfig;

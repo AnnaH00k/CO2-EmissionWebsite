144c597df07bd4d2d42ccf5a94a24ee60b9892b6
Anna H00k <annahook0@gmail.com>
2024-04-21 16:56:42 +0200

    make footer github pages compatible

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -1,14 +1,27 @@
 "use client";
 
+import { useEffect } from "react";
+
 export default function Footer() {
+    useEffect(() => {
+        if (window.location.hostname === 'annah00k.github.io') {
+            const links = document.querySelectorAll('.github-page-link');
+            links.forEach(link => {
+                const href = link.getAttribute('href');
+                if (href) {
+                    link.setAttribute('href', '/CO2-EmissionWebsite' + href);
+                }
+            });
+        }
+    }, []);
 
     return (
-    <footer className="flex my-[10vh] mx-2 justify-center">
-        <ul className="">
-            <a className="m-2 text-blueExtraLight" href="../datasecurity"> Datasecurity</a>
-            <a className="m-2 text-blueExtraLight"  href="../impressum"> Imprint</a>
-            <a className="m-2 text-blueExtraLight"  href="../contact">Contact</a>
-        </ul>
-    </footer>
+        <footer className="flex my-[10vh] mx-2 justify-center">
+            <ul className="">
+                <a className="m-2 text-blueExtraLight github-page-link" href="/datasecurity"> Datasecurity</a>
+                <a className="m-2 text-blueExtraLight github-page-link" href="../impressum"> Imprint</a>
+                <a className="m-2 text-blueExtraLight github-page-link" href="../contact">Contact</a>
+            </ul>
+        </footer>
     );
 }

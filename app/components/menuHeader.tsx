19097daa13a0f6788da42cc97e34a02eaec57dec
Anna H00k <annahook0@gmail.com>
2024-04-21 18:29:45 +0200

    put effect on top

--- app/components/menuHeader.tsx
+++ app/components/menuHeader.tsx
@@ -13,6 +13,19 @@ export default function MenuHeader() {
 
 
     useEffect(() => {
+        if (window.location.hostname === 'annah00k.github.io') {
+            const links = document.querySelectorAll('.github-page-link');
+            links.forEach(link => {
+                const href = link.getAttribute('href');
+                if (href) {
+                    link.setAttribute('href', href.startsWith('/CO2-EmissionWebsite') ? href : '/CO2-EmissionWebsite' + href);
+                }
+            });
+        }
+    }, []);
+
+
+    useEffect(() => {
         function handleClickOutside(event: MouseEvent) {
             if (menuRef.current && !(menuRef.current as any).contains(event.target)) {
                 setMenu1Open(false);
@@ -69,18 +82,7 @@ export default function MenuHeader() {
 
 
 
-    useEffect(() => {
-        if (window.location.hostname === 'annah00k.github.io') {
-            const links = document.querySelectorAll('.github-page-link');
-            links.forEach(link => {
-                const href = link.getAttribute('href');
-                if (href) {
-                    link.setAttribute('href', href.startsWith('/CO2-EmissionWebsite') ? href : '/CO2-EmissionWebsite' + href);
-                }
-            });
-        }
-    }, []);
-    
+
 
     return (
         <header>

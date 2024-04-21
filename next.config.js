6979ef53936c93f9e41d7de13a944ce9e75f18c1
Anna H00k <annahook0@gmail.com>
2024-04-21 03:02:24 +0200

    redo not working freecodecamp tutorial

--- next.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    basePath: "/co2-emissionwebsite",
-    output: "export",
-    reactStrictMode: true,
-  };
-  
-  module.exports = nextConfig;
